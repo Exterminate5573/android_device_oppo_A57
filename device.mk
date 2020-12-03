17b058fad480573157457151549e5f533e3bdfe4
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    A57: Disable the usage of ConfigStore.
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- device.mk
+++ device.mk
@@ -141,6 +141,10 @@ PRODUCT_PACKAGES += \
     libstdc++_vendor \
     android.frameworks.displayservice@1.0.vendor
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
