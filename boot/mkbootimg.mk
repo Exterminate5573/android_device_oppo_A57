310e7f38048784ec4d7c6faee0b61215476700b3
Aaron Kling <webgeek1234@gmail.com>
2022-04-20 18:32:37 -0500

    A57: mkbootimg: Update custom boot image rule for S
    Change-Id: Ieb3f40b45e9ecd71f1c15a966cd2e35cc2f983bc

--- boot/mkbootimg.mk
+++ boot/mkbootimg.mk
@@ -23,9 +23,9 @@ BOOT_PATCH := $(DEVICE_PATH)/boot
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_KERNEL_TARGET)
 	$(call pretty, "Target boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$@) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) cat $(BOOT_PATCH)/boot.patch >> $@
-	$(hide) $(call assert-max-image-size, $@, $(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	$(hide) $(call assert-max-image-size, $@, $(call get-bootimage-partition-size,$@,boot))
 	@echo "Made boot image: $@"
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
