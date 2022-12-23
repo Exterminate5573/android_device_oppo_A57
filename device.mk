082fca10dc8f3ad94141c09f878b5386c2ae33c2
Bharath <bharath@teamb58.org>
2022-12-23 12:39:58 +0530

    A57: Revert "A57: Adding GSI keys"
    This reverts commit 74f7622c9f926e7da06e3fbe448775740fd53e4b.
    
    * Not in stock anyways.
    * A57 has no first stage ramdisk and hence GSI keys are not required.
    
    Change-Id: I3fffe58b71ac8f32844a70562c9a3e2e3d5b10da

--- device.mk
+++ device.mk
@@ -9,9 +9,6 @@ DEVICE_PATH := device/oppo/A57
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
