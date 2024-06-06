eb2ac22d8932881d45f621c897bf7e441840c7cc
Ricky Cheung <rcheung844@gmail.com>
2024-06-06 11:09:19 +0800

    msm8937-common: Build OSS camera HAL
    Change-Id: Ic06a66ff433a30dcdac1627ac7e179b595b3c5e0

--- msm8937.mk
+++ msm8937.mk
@@ -105,6 +105,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.msm8937 \
+    libmm-qcamera
+
+PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
