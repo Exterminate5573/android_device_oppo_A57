14f6bc8733c6e78483986b26a3f99116bb9062a3
Ricky Cheung <rcheung844@gmail.com>
2025-05-11 14:30:42 +0800

    A57: Properly migrate all camera blobs to /data/vendor/qcam
    The migration in 5048b52ad was not complete. Finish the migration by
    hex editing all blobs to use the custom path.
    
    Change-Id: I6c4c95513cab3417d66c43f575ed1dfc3c7cdee2

--- extract-files.sh
+++ extract-files.sh
@@ -62,9 +62,6 @@ function blob_fixup() {
             "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
             "${PATCHELF}" --replace-needed "libqdMetaData.so" "libqdMetaData.system.so" "${2}"
             ;;
-        vendor/bin/mm-qcamera-daemon)
-            sed -i "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" "${2}"
-            ;;
         vendor/bin/rmt_storage)
             # Drop IO priority set-up
             sed -i "s|\xe0\x0f\x1f\x32|\x0e\x00\x00\x14|g" "${2}"
@@ -78,17 +75,24 @@ function blob_fixup() {
         vendor/lib64/libalipay_factory.so|vendor/lib64/lib_fpc_tac_shared.so)
             sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
             ;;
-        vendor/lib/libmmcamera2_sensor_modules.so)
-            sed -i "s/\/system\/etc\/camera\//\/vendor\/etc\/camera\//g" "${2}"
-            ;;
-        vendor/lib/libmmcamera2_stats_modules.so)
-            sed -i "s|libandroid.so|libcamshim.so|g" "${2}"
-            "${PATCHELF}" --replace-needed "libgui.so" "libgui_vendor.so" "${2}"
-            ;;
         vendor/lib64/libril-qc-hal-qmi.so)
             for v in 1.{0..2}; do
                 sed -i "s|android.hardware.radio.config@${v}.so|android.hardware.radio.c_shim@${v}.so|g" "${2}"
             done
+            ;;
+        vendor/bin/mm-qcamera-daemon|vendor/lib/libmmcamera2_cpp_module.so|vendor/lib/libmmcamera2_dcrf.so|vendor/lib/libmmcamera2_iface_modules.so|vendor/lib/libmmcamera2_imglib_modules.so|vendor/lib/libmmcamera2_mct.so|vendor/lib/libmmcamera2_pproc_modules.so|vendor/lib/libmmcamera2_q3a_core.so|vendor/lib/libmmcamera2_stats_algorithm.so|vendor/lib/libmmcamera_imglib.so|vendor/lib/libmmcamera_pdaf.so|vendor/lib/libmmcamera_pdafcamif.so|vendor/lib/libmmcamera_tintless_algo.so|vendor/lib/libmmcamera_tintless_bg_pca_algo.so|vendor/lib/libmmcamera_tuning.so|vendor/lib/libremosaic_daemon.so)
+            sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "${2}"
+            ;;
+        vendor/lib/libmmcamera_dbg.so)
+            sed -i "s|/data/vendor/camera/|///data/vendor/qcam/|g" "${2}"
+            ;;
+        vendor/lib/libmmcamera2_sensor_modules.so)
+            sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "${2}"
+            sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "${2}"
+            ;;
+        vendor/lib/libmmcamera2_stats_modules.so)
+            sed -i "s|libandroid.so|libcamshim.so|g" "${2}"
+            "${PATCHELF}" --replace-needed "libgui.so" "libgui_vendor.so" "${2}"
             ;;
         vendor/lib/libchromaflash.so|vendor/lib/libcvface_api.so|vendor/lib/liboptizoom.so|vendor/lib/libmmcamera_hdr_gb_lib.so|vendor/lib/libts_detected_face_hal.so|vendor/lib/libts_face_beautify_hal.so|vendor/lib/libseemore.so|vendor/lib/libtrueportrait.so|vendor/lib/libubifocus.so)
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
